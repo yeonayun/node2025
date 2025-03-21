0e699ca4f4cc8967582e3b8a8884cc01a684cac7
yeonayun <w2306@e-mirim.hs.kr>
2025-03-21 12:04:30 +0900

    parameter 요청

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.post('/swag', (req, res) => {
   res.send(req.body);
 });
 
+app.post('/swag/:person', (req, res) => {
+  // 클라이언트로 보낼 메시지
+  res.send(req.params.person);
+});
+
 // 서버가 3000번 포트에서 요청을 기다립니다
 app.listen(2007, () => {
   console.log(`Server running at http://localhost:${port}}/`);
