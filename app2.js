db9c24b32befb45df00c94437e9a111183b9e927
yeonayun <w2306@e-mirim.hs.kr>
2025-04-02 10:18:32 +0900

    Read(Get)

--- app2.js
+++ app2.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
+const dotenv = require('dotenv')
+dotenv.config();
 
 const app = express();
 const port = 2007;
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect(err => {
+  if(err) {
+    console.error('MySQL 연결 실패: ', err);
+    return;
+  }
+  console.log('MySQL 연결 성공');
+})
+
 app.set('view engine', 'ejs');
 
 // __dirname : 현재 파일이 속해있는 디렉토리의 절대경로
@@ -14,10 +33,19 @@ console.log(__dirname + '\\views');
 const travelList = ['뉴욕', '파리', '우리집', '하와이', '영호집', '별이집'];
 
 app.get('/travel', (req, res) => {
-  res.render('travel', { travelList });
+  const _query = 'SELECT * FROM travellist'
+  db.query(_query, (err, results) => {
+    if(err){
+      console.error('데이터베이스 쿼리 실패: ', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    const travelList = results;
+    res.render('travel', { travelList });
+  });
 });
 
-app.listen(2007, () => {
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}}/`);
 });
 
