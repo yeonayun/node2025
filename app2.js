2443a1096aaeedec9df10272c4ffeeaaad035eef
yeonayun <w2306@e-mirim.hs.kr>
2025-04-09 10:22:28 +0900

    Update(PUT)프론드엔드

--- app2.js
+++ app2.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql2');
+const methodOverride = require('method-override')
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -23,6 +24,7 @@ db.connect(err => {
   console.log('MySQL 연결 성공!!');
 })
 
+app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
@@ -77,10 +79,6 @@ app.post('/travel', (req, res) => {
   });
 })
 
-app.get('/add-travel',(req,res) => {
-    res.render('addTravel');
-});
-
 app.put('/travel/:id', (req, res) => {
     const travelId = req.params.id;
     const {name} = req.body;
@@ -95,7 +93,25 @@ app.put('/travel/:id', (req, res) => {
     });
   });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+  app.get('/travel/:id/edit', (req, res) => {
+    const travelId = req.params.id;
+    const _query = 'SELECT * FROM travellist WHERE id = ?';
+    db.query(_query, [travelId], (err, results) => {
+      if(err) {
+        console.error('데이터베이스 쿼리 실패 : ', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      const travel = results[0];
+      res.render('editTravel', { travel });
+    });
+  });
+
+  app.get('/add-travel',(req,res) => {
+    res.render('addTravel');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+  });
 
