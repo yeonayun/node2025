df0332601b0ad289af343620b641879d159dde1f
yeonayun <w2306@e-mirim.hs.kr>
2025-04-16 16:12:12 +0900

    기능 주석 추가

--- routes/travel.js
+++ routes/travel.js
@@ -2,23 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/add', (req, res)=>{
-  res.render('addTravel');
-})
-
-router.get('', (req, res) => {
-  const _query = 'SELECT * FROM travellist';
-  db.query(_query, (err, results) => {
-    if(err){
-      console.error('데이터베이스 쿼리 실패', err);
-      res.status(500).send('Internal Server Error');
-      return;
-    }
-    const travelList = results;
-    res.render('travel', {travelList});
-  });
-});
-
+// 전체 게시글 목록 보여주는 페이지
 router.get('/:id', (req, res) => {
   const travelId = req.params.id;
   const _query = 'SELECT id, name FROM travellist WHERE id = ?';
@@ -34,6 +18,25 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.get('/add', (req, res)=>{
+  res.render('addTravel');
+})
+
+// 게시글의 내용을 읽기
+router.get('', (req, res) => {
+  const _query = 'SELECT * FROM travellist';
+  db.query(_query, (err, results) => {
+    if(err){
+      console.error('데이터베이스 쿼리 실패', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    const travelList = results;
+    res.render('travel', {travelList});
+  });
+});
+
+// 여행지 추가를 보여주는 페이지
 router.post('', (req, res) => {
   const {name} = req.body;
   const _query = 'INSERT INTO travellist (name) VALUES (?)';
@@ -47,6 +50,7 @@ router.post('', (req, res) => {
   });
 })
 
+// 게시글 수정
 router.put('/:id', (req, res) => {
   const travelId = req.params.id;
   const {name} = req.body;
@@ -61,6 +65,7 @@ router.put('/:id', (req, res) => {
   });
 });
 
+// 게시글 수정 페이지
 router.get('/:id/edit', (req, res) => {
   const travelId = req.params.id;
   const _query = 'SELECT * FROM travellist WHERE id = ?';
@@ -79,6 +84,7 @@ router.get('/add', (req, res) => {
   res.render('addTravel')
 });
 
+// 게시글 삭제
 router.delete('/:id', (req, res) => {
   const travelId = req.params.id;
   const _query = 'DELETE FROM travelList WHERE id = ?';
